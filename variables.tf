c94bc53be67ac81a9685c48b21d4cd698fdbb65e
hkacmaz <halilkacmaz@outlook.com>
2024-11-25 13:41:51 +0100

    added ami_id instance_type onto variables.tf

--- variables.tf
+++ variables.tf
@@ -44,3 +44,15 @@ variable "record_name" {
   description = "Name of the DNS record"
   type        = string
 }
+
+variable "ami_id" {
+  description = "ID of the AMI to use for the EC2 instance"
+  type        = string
+  default     = "ami-0d52a4bd932508da0"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to create"
+  type        = string
+  default     = "t2.micro"
+}
